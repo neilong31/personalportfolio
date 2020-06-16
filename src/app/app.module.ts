be12adba3bbf0b1010ce68538b65e08379ec60ec
Neil <neil.ong31@gmail.com>
2020-06-16 00:35:51 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  exports: [
+    MatCardModule,
+  ],
+  declarations: [],
+  imports: [BrowserAnimationsModule]
+})
+export class MaterialModule {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    MDBBootstrapModule.forRoot(),
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
